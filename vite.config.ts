1e35987fbf2366514052a46412f14d50e16d9bab
SolomonLeon <20776590+SolomonLeon@users.noreply.github.com>
2025-02-04 16:33:45 +0800

    UPDATE: Add rollup visualization & Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import wasmPack from "vite-plugin-wasm-pack";
 import Pages from "vite-plugin-pages";
 
+import { visualizer } from "rollup-plugin-visualizer";
+
 export default defineConfig({
   base: "./",
   plugins: [
@@ -22,6 +24,13 @@ export default defineConfig({
         }
         return "async";
       },
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: "rollup_visualization.html", //分析图生成的文件名
+      open: true, //如果存在本地服务端口，将在打包后自动展示
     }),
   ],
 });
