54acb1f150e51db689f165aa75f5170e569796a7
SolomonLeon <20776590+SolomonLeon@users.noreply.github.com>
2025-03-25 23:22:02 +0800

    UPDATE: G1 0.4B

--- src/targets/rwkv-hf-space/rwkv-hf-space-models.ts
+++ src/targets/rwkv-hf-space/rwkv-hf-space-models.ts
@@ -8,6 +8,36 @@ import { t } from "@lingui/core/macro";
 
 export const RWKV_G1_MODELS: APIModel[] = [
   {
+    title: "RWKV7 G1 0.4B",
+    name: "rwkv7-g1-0.4b-20250324-ctx4096",
+    description: t`A tiny but impressive reasoning model with only 0.4B parameters.`,
+    supportReasoning: true,
+    reasoningName: "rwkv7-g1-0.4b-20250324-ctx4096:thinking",
+    param: "0.4B",
+    dataset: "v2.8",
+    update: "2025/03/24",
+    ctx: "4096",
+    defaultSessionConfiguration: {
+      stopTokens: DEFAULT_STOP_TOKENS,
+      stopWords: DEFAULT_STOP_WORDS,
+      maxTokens: 8192,
+      systemPrompt: null,
+      defaultSamplerConfig: {
+        temperature: 2.0,
+        top_p: 0.3,
+        presence_penalty: 0.5,
+        count_penalty: 0.5,
+        half_life: 200,
+      },
+    },
+    APIParam: {
+      baseUrl: import.meta.env.VITE_API_MODEL_BASE_URL,
+      key: "sk-test",
+    },
+    from: "API",
+    defaultMode: "reasoning",
+  },
+  {
     title: "RWKV7 G1 0.1B",
     name: "rwkv7-g1-0.1b-20250307-ctx4096",
     description: t`A tiny but impressive reasoning model with only 0.1B parameters. `,
@@ -81,36 +111,6 @@ export const AVALIABLE_TEMP_HF_MODELS: APIModel[] = [
     param: "1.5B",
     dataset: "v2.8",
     update: "2025/03/19",
-    ctx: "4096",
-    defaultSessionConfiguration: {
-      stopTokens: DEFAULT_STOP_TOKENS,
-      stopWords: DEFAULT_STOP_WORDS,
-      maxTokens: 4096,
-      systemPrompt: null,
-      defaultSamplerConfig: {
-        temperature: 2.0,
-        top_p: 0.3,
-        presence_penalty: 0.5,
-        count_penalty: 0.5,
-        half_life: 200,
-      },
-    },
-    APIParam: {
-      baseUrl: import.meta.env.VITE_API_MODEL_BASE_URL,
-      key: "sk-test",
-    },
-    from: "API",
-    defaultMode: "reasoning",
-  },
-  {
-    title: "RWKV7 G1 0.4B 75% trained",
-    name: "RWKV7-G1-0.4B-75%trained-20250317-ctx4k",
-    description: t`A tiny but impressive reasoning model with only 0.4B parameters.`,
-    supportReasoning: true,
-    reasoningName: "RWKV7-G1-0.4B-75%trained-20250317-ctx4k:thinking",
-    param: "0.4B",
-    dataset: "v2.8",
-    update: "2025/03/17",
     ctx: "4096",
     defaultSessionConfiguration: {
       stopTokens: DEFAULT_STOP_TOKENS,
